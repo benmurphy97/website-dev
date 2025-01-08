c834c1468b729223684ac7309689658e2e59f4be
Ben <ben.murphy@live.ie>
2025-01-08 17:34:09 +1100

    data cleaning routes

--- app/routes.py
+++ app/routes.py
@@ -123,13 +123,13 @@ def scrape_match_data():
     # links_df, scores = get_links_df(base_url)
     # print(links_df.head())
 
-    # matches_df = scrape_matches_from_links(links_df, scores)
-    # print(matches_df.head())
+    # df = scrape_matches_from_links(links_df, scores)
+    # print(df.head())
 
-    # matches_df.to_csv('matches.csv')
+    # df.to_csv('matches.csv', index=False)
     print("loading data from csv")
     df = pd.read_csv('matches.csv')
-    df = df.drop(columns=['Unnamed: 0'])
+
     print(df.dtypes)
 
     df['league'] = league
@@ -138,8 +138,46 @@ def scrape_match_data():
     df['match_date_dt'] = pd.to_datetime(df['match_date'])
     df['match_date_str'] = df['match_date_dt'].dt.strftime("%Y-%m-%d")
 
-    cols_to_write = ['match_date_str', 'season', 'league', 'home_team', 'away_team', 'match_result']
-    print(df[cols_to_write].head())
+    import re
+    df['home_score'] = df['match_result'].apply(lambda x: re.findall(r'[\d]+', x)[0]).astype(int)
+    df['away_score'] = df['match_result'].apply(lambda x: re.findall(r'[\d]+', x)[1]).astype(int)
+
+
+    def match_outcome(h,a):
+        if h > a: # home win
+            if (h - a) > 7:
+                return 2
+            else:
+                return 1 # away team gets bonus pt
+            
+        elif h < a: # away win
+            if (a - h) > 7:
+                return -2
+            else:
+                return -1 # home team gets bonus pt
+        else:
+            return 0
+    
+    df['long_outcome'] = df.apply(lambda x: match_outcome(x.home_score, x.away_score), axis=1)
+
+    import datetime
+    df.loc[df['match_date_dt'] >= datetime.datetime.today(), 'home_score'] = 0
+    df.loc[df['match_date_dt'] >= datetime.datetime.today(), 'away_score'] = 0
+
+    cols_to_write = ['match_date_str', 'season', 'league', 'home_team', 'away_team', 'link',
+                     
+                     'match_result', 'home_score', 'away_score', 'long_outcome',
+                     
+                     'home_n_tries', 'away_n_tries',
+                     'home_n_conversions', 'away_n_conversions',
+                     'home_n_pen_kicks', 'away_n_pen_kicks',
+                     'home_n_pen_tries', 'away_n_pen_tries'
+                     ]
+    
+
+    df.fillna(0, inplace=True)
+
+    print(df[cols_to_write].tail())
 
 
     write_to_postgres_table(df[cols_to_write], table='matches_raw')
